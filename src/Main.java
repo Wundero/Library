540b3b4eb6eb3888191055a1c7b9d524b3488a45
Wundero <sublimecraftftw@gmail.com>
2016-04-15 20:02:18 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
 public class Main {
-
+  //args are names of books
+    public static void main(String[] args) {
+      Database db = new Database();
+      System.out.println("Database initiated.");
+      for(String book : args) {
+        db.addBook(book);
+      }
+      System.out.println(db);
+    }
 }
